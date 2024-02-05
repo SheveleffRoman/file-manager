d42699ec1445b80a24fd86ab4261cadeb5319427
SheveleffRoman <@>
2024-02-05 15:39:52 +0300

    feat: add relative and absolute paths

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,11 +1,16 @@
 import fs from "fs/promises";
 import path from "path";
 
-export const create = async (pathToFile, filename) => {
-  const fileName = filename.split(" ").slice(1).join(" ");
-  const createPath = path.join(pathToFile, fileName);
+export const create = async (currentDir, pathToFile) => {
+  let createPath;
+  if (pathToFile.startsWith("/")) {
+    createPath = path.join(pathToFile);
+  } else {
+    createPath = path.join(currentDir, pathToFile);
+  }
   try {
     await fs.writeFile(createPath, "", { flag: "wx" });
+    console.log(`You are currently in ${currentDir}`);
   } catch {
     console.log("Operation failed");
   }
