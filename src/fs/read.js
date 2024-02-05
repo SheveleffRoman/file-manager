0510250fb65ee934dd3548e24cee287484075297
SheveleffRoman <@>
2024-02-05 15:24:54 +0300

    fix: add relative and absolute path

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,8 +1,13 @@
 import path from "path";
 import { createReadStream } from "node:fs";
 
-export const read = async (pathToFile, filename) => {
-  const createPath = path.join(pathToFile, filename);
+export const read = async (currentDir, pathToFile) => {
+  let createPath;
+  if (pathToFile.startsWith("/")) {
+    createPath = path.join(pathToFile);
+  } else {
+    createPath = path.join(currentDir, pathToFile);
+  }
   try {
     const readStream = createReadStream(createPath).on("error", () =>
       console.log("Operation failed")
@@ -10,9 +15,6 @@ export const read = async (pathToFile, filename) => {
     readStream.setEncoding("utf-8");
     readStream.on("data", (data) => {
       console.log(data);
-    });
-    readStream.on("error", () => {
-      console.log("Operation failed");
     });
   } catch (err) {
     console.log("Operation failed");
