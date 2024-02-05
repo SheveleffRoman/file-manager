f477ebf54cbc35e9175a0334a07421cd81318318
SheveleffRoman <@>
2024-02-05 14:40:04 +0300

    fix: handle error

--- src/fs/read.js
+++ src/fs/read.js
@@ -4,7 +4,9 @@ import { createReadStream } from "node:fs";
 export const read = async (pathToFile, filename) => {
   const createPath = path.join(pathToFile, filename);
   try {
-    const readStream = createReadStream(createPath);
+    const readStream = createReadStream(createPath).on("error", () =>
+      console.log("Operation failed")
+    );
     readStream.setEncoding("utf-8");
     readStream.on("data", (data) => {
       console.log(data);
