d19f96490b08379c94c01d698dcba660406e2681
SheveleffRoman <@>
2024-02-05 17:34:52 +0300

    feat: add absolute and relative paths

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,8 +3,16 @@ import { createReadStream, createWriteStream } from "node:fs";
 
 export const copy = async (currentDir, paths) => {
   const pathArr = paths.split(" ");
-  const pathToFile = path.join(currentDir, pathArr[0]);
-  const newPath = path.join(pathArr[1], pathArr[0]);
+  let pathToFile;
+  let newPath;
+
+  if (pathArr[0].startsWith("/") && pathArr[0].startsWith("/")) {
+    pathToFile = path.join(pathArr[0]);
+    newPath = path.join(pathArr[1], pathArr[0].split("/").slice(-1).join());
+  } else {
+    pathToFile = path.join(currentDir, pathArr[0]);
+    newPath = path.join(pathArr[1], pathArr[0]);
+  }
 
   try {
     const readStream = createReadStream(pathToFile).on("error", () =>
@@ -13,7 +21,9 @@ export const copy = async (currentDir, paths) => {
     const writeStream = createWriteStream(newPath).on("error", () =>
       console.log("Operation failed")
     );
-    readStream.pipe(writeStream);
+    readStream
+      .pipe(writeStream)
+      .on("finish", () => console.log(`You are currently in ${currentDir}`));
   } catch {
     console.log("Operation failed");
   }
